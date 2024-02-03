e5cd5ca2c9b3a5be12664271956ba14c2d39efdf
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-03 02:26:27 -0600

    Test: display player count

--- spec/features/dnd_campaigns/show_spec.rb
+++ spec/features/dnd_campaigns/show_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
-# As a visitor
-# When I visit '/parents/:id'
-# Then I see the parent with that id including the parent's attributes
-# (data from each column that is on the parent table)
 
 RSpec.describe "DnD Campaigns Show Page" do
   let!(:campaign1) {DndCampaign.create!(name: "Ravenloft", setting: "Forgotten Realm", active_campaign: true, sessions: 15)}
   let!(:campaign2) {DndCampaign.create!(name: "Mighty Nein", setting: "Castles", active_campaign: false, sessions: 152)}
 
-  describe "when user visits show page" do
+  describe "when user visits" do
     before(:each) do
       visit "/dnd_campaigns/#{campaign1.id}"
     end
@@ -32,6 +28,11 @@ RSpec.describe "DnD Campaigns Show Page" do
     it "displays campaign's session count" do
       expect(page).to have_content(campaign1.sessions)
       expect(page).to_not have_content(campaign2.sessions)
+    end
+
+    it "displays campaign's players count" do
+      expect(page).to have_content(campaign1.player_count)
+      expect(page).to_not have_content(campaign2.player_count)
     end
   end
 end
