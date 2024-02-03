7380e061eb7495c15f61a17cb1d136fde52069e0
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-03 02:34:37 -0600

    Test: count players

--- spec/models/dnd_campaign_spec.rb
+++ spec/models/dnd_campaign_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe DndCampaign, type: :model do
   describe "relationships" do
     it {should have_many :players}
   end
+
+  describe "instance methods" do
+    let!(:campaign1) {DndCampaign.create!(name: "Ravenloft", setting: "Forgotten Realm", active_campaign: true, sessions: 15)}
+    let!(:player1) {campaign1.players.create!(name: "Jenna", game_master: true, character_name: "NPCs", sessions_missed: 0)}
+    let!(:player2) {campaign1.players.create!(name: "Amy", game_master: false, character_name: "Francis", sessions_missed: 5)}
+    
+    it "counts players" do
+      expect(campaign1.player_count).to eq(2)
+    end
+  end
 end
