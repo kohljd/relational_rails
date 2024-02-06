480822c3790d5218c79832373116050ab7072eb6
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-06 04:21:45 -0600

    Test: update player

---
+++ spec/features/players/edit_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Player's Edit Page" do
+  describe "when user visits" do
+    let!(:campaign1) {DndCampaign.create!(name: "Kingmaker", setting: "Stolen Lands", active_campaign: true, sessions: 18)}
+    let!(:player1) {campaign1.players.create!(name: "Jenna", game_master: true, character_name: "NPCs", sessions_missed: 0)}
+  
+    describe "when user visits" do
+      before do
+        visit "/players/#{player1.id}/edit"
+      end
+
+      it 'displays player form with current player info' do
+        save_and_open_page
+        expect(page.find_field("Name").value).to eq(player1.name)
+        expect(page.find_field("Character Name").value).to eq(player1.character_name)
+        expect(page.find_field("Sessions Missed").value.to_i).to eq(player1.sessions_missed)
+        expect(page.find_field("Game Master").value).to eq(player1.game_master.to_s)
+      end
+      
+      it "updated form redirects to Players Show Page " do
+        fill_in "Character Name", with: "Velma"
+        select "false", from: "Game Master"
+        
+        click_button "Submit Changes"
+        expect(current_path).to eq("/players/#{campaign1.id}")
+        save_and_open_page
+        expect(page).to have_content(Velma)
+        expect(page).to have_content("false")
+      end
+    end
+  end
+end
